84d4970b10b2c75c9f67a2320194548a20574415
Abdulhamid254 <nassurabdulhamid49@gmail.com>
2023-09-19 11:40:22 -0700

    Updated hero section

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -3,6 +3,8 @@
 import  { Cursor, useTypewriter } from "react-simple-typewriter"
 import BackgroundCircle from "./BackgroundCircle"
 import Image from "next/image";
+import Link from 'next/link'
+
 
 
 type Props ={}
@@ -40,10 +42,30 @@ export default function Hero({}: Props) {
       />
       
       <div>
-        <h1>
-           <span>{text}</span>
+        <h2 className="text-sm uppercase text-gray-500 pb-2 tracking-[15px]">
+             Software Engineer</h2>
+        <h1 className="text-5xl lg:text-6xl font-semibold px-18">
+           <span className="mr-3">{text}</span>
            <Cursor cursorColor='#F7AB0A'/>
         </h1> 
+
+        <div className="pt-5">
+            <Link href='#about'>
+            <button className="heroButton">About</button>
+            </Link>
+
+            <Link href='#experience'>
+            <button className="heroButton">Experience</button>
+            </Link>
+
+            <Link href='#skills'>
+            <button className="heroButton">Skills</button>
+            </Link>
+
+            <Link href='#projects'>
+            <button className="heroButton">Projects</button>
+            </Link>
+        </div>
         </div> 
     </div>
 </div>
